7dd3871c36a81b039f30c18a89a36fdeca39f78b
RoED <nameeduard@yandex.ru>
2021-02-22 11:55:44 +0300

    jsonpath

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,6 +1,14 @@
-
+import com.jayway.jsonpath.JsonPath
+import java.io.File
 
 fun main() {
+    var file = File("src/main/resources/themes/forTest.json")
 
-print("dsa")
+
+    print(
+
+    JsonPath.read<String>(file, "$..name")
+
+
+    )
 }
